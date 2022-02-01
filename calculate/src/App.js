dca48df76654765592b4c43297dbebca0382210d
tokagi7428 <79756474+tokagi7428@users.noreply.github.com>
2022-02-01 22:23:35 +0700

    modified width

--- calculate/src/App.js
+++ calculate/src/App.js
@@ -3,7 +3,7 @@ import Calculate from "./component/Calculate";
 import "./App.css";
 function App() {
   return (
-    <Container className="mt-5 border bordered w-50 bg-dark p-2 container-md">
+    <Container className="mt-5 border bordered w-25 bg-dark p-2 container-md">
       <h1 className="fs-0 m-2 text-center text-light">Calculate</h1>
       <Calculate />
     </Container>
